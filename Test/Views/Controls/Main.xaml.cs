ce83d6839ae6e7c5bdc9df30ac537178670f3eba
Little language <47820811+Sereoj@users.noreply.github.com>
2021-01-02 19:31:14 +0300

    Добавление UserControl в качестве основного окна

--- Test/Views/Controls/Main.xaml.cs
+++ Test/Views/Controls/Main.xaml.cs
@@ -12,24 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Test.Views.Controls;
 
-namespace Test
+namespace Test.Views.Controls
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для Main.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Main : UserControl
     {
-        public MainWindow()
+        public Main()
         {
             InitializeComponent();
-        }
-
-        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if(e.LeftButton == MouseButtonState.Pressed)
-                DragMove();
         }
     }
 }
