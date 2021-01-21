9792602b7af99c5eb1f9ac68820572227f7f31bc
Little language <47820811+Sereoj@users.noreply.github.com>
2021-01-21 17:06:03 +0300

    Начало исправления обновления приложения.

---
+++ Test.Tests/GLUpdateTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Test.Tests.Modules;
+
+namespace Test.Tests
+{
+    [TestClass]
+    public class GLUpdateTest
+    {
+        [TestMethod]
+        public void IsUpdateTrue()
+        {
+             GLUpdater.Model.Checker();
+             Assert.IsTrue(GLUpdater.Model.IsUpdate());
+
+        }
+    }
+}
---
+++ TestUpdateConsole/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestUpdateConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+            using (var client = new WebClient())
+            {
+                try
+                {
+                    
+                    var getNewVersion = client.OpenRead(new Uri("https://raw.githubusercontent.com/Sereoj/uploads/main/ds_new/version.txt", UriKind.Absolute));
+
+                    using (StreamReader StreamReader = new StreamReader(getNewVersion))
+                    {
+                        Console.WriteLine(StreamReader.ReadToEnd().Trim());
+                        StreamReader.Close();
+                    }
+
+                }
+                catch (WebException wex)
+                {
+                    Console.WriteLine(wex.Message);
+                }
+                client.Dispose();
+            }
+            Console.ReadLine();
+        }
+    }
+}
