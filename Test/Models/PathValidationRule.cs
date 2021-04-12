4dcc7bff182aaeda0b8bbc43b01943445fbdcf28
Sergey Egorov <ser.12egorov@gmail.com>
2021-04-12 21:41:28 +0300

    Исправление

---
+++ Test/Models/PathValidationRule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Test.Models
+{
+    public class PathValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
