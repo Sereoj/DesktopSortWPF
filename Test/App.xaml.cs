680df65587c845d013e167a6f795a84f857f3a77
Sergey Egorov <ser.12egorov@gmail.com>
2021-06-18 17:06:31 +0300

    Аналитика сбоев

--- Test/App.xaml.cs
+++ Test/App.xaml.cs
@@ -3,6 +3,9 @@ using Test.Models;
 using Test.Models.Settings;
 using Test.ViewModels;
 using Test.ViewModels.Base;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace Test
 {
@@ -13,6 +16,9 @@ namespace Test
     {
         protected override void OnStartup(StartupEventArgs e)
         {
+            AppCenter.Start("e451f2c8-a137-4826-8dd8-41c61cee8c87",
+                   typeof(Analytics), typeof(Crashes));
+
         }
     }
 }
