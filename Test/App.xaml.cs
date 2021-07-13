ab467ea4cf324ea7070fb9d5df9eaa29a19e73b4
Sergey Egorov <ser.12egorov@gmail.com>
2021-07-13 13:46:52 +0300

    Обновление пакетов

--- Test/App.xaml.cs
+++ Test/App.xaml.cs
@@ -1,11 +1,4 @@
 ﻿using System.Windows;
-using Test.Models;
-using Test.Models.Settings;
-using Test.ViewModels;
-using Test.ViewModels.Base;
-using Microsoft.AppCenter;
-using Microsoft.AppCenter.Analytics;
-using Microsoft.AppCenter.Crashes;
 
 namespace Test
 {
@@ -16,9 +9,6 @@ namespace Test
     {
         protected override void OnStartup(StartupEventArgs e)
         {
-            AppCenter.Start("e451f2c8-a137-4826-8dd8-41c61cee8c87",
-                   typeof(Analytics), typeof(Crashes));
-
         }
     }
 }
